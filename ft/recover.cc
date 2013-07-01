cbd4564de8893496c2f3891946a8989e4e18396f
Leif Walsh <leif.walsh@gmail.com>
2013-07-01 14:08:01 -0400

    clean up use of ft layer thunks for generate_rows_for_{put,del}, and fix a few errors #26

--- ft/recover.cc
+++ ft/recover.cc
@@ -1018,23 +1018,6 @@ static int toku_recover_backward_enq_delete_any (struct logtype_enq_delete_any *
     return 0;
 }
 
-struct recover_enq_insert_multiple_row_processor {
-    struct file_map_tuple *m_tuple;
-    TOKUTXN m_txn;
-    struct logtype_enq_insert_multiple *m_l;
-    recover_enq_insert_multiple_row_processor(struct file_map_tuple *tuple, TOKUTXN txn, struct logtype_enq_insert_multiple *l)
-        : m_tuple(tuple), m_txn(txn), m_l(l) {}
-
-    int process(DBT *dest_key, DBT *dest_val) {
-        toku_ft_maybe_insert(m_tuple->ft_handle, dest_key, dest_val, m_txn, true, m_l->lsn, false, FT_INSERT);
-        return 0;
-    }
-    static int call_process(void *extra, DBT *dest_key, DBT *dest_val) {
-        recover_enq_insert_multiple_row_processor *e = static_cast<recover_enq_insert_multiple_row_processor *>(extra);
-        return e->process(dest_key, dest_val);
-    }
-};
-
 static int toku_recover_enq_insert_multiple (struct logtype_enq_insert_multiple *l, RECOVER_ENV renv) {
     int r;
     TOKUTXN txn = NULL;
@@ -1068,11 +1051,28 @@ static int toku_recover_enq_insert_multiple (struct logtype_enq_insert_multiple 
             if (r==0) {
                 // We found the cachefile.  (maybe) Do the insert.
                 DB *db = &tuple->fake_db;
-                recover_enq_insert_multiple_row_processor processor(tuple, txn, l);
+
+                struct recover_enq_insert_multiple_row_processor {
+                    struct file_map_tuple *m_tuple;
+                    TOKUTXN m_txn;
+                    struct logtype_enq_insert_multiple *m_l;
+                    recover_enq_insert_multiple_row_processor(struct file_map_tuple *tuple, TOKUTXN txn, struct logtype_enq_insert_multiple *l)
+                        : m_tuple(tuple), m_txn(txn), m_l(l) {}
+
+                    int process(DBT *dest_key, DBT *dest_val) {
+                        toku_ft_maybe_insert(m_tuple->ft_handle, dest_key, dest_val, m_txn, true, m_l->lsn, false, FT_INSERT);
+                        return 0;
+                    }
+                    static int call_process(void *extra, DBT *dest_key, DBT *dest_val) {
+                        recover_enq_insert_multiple_row_processor *e = static_cast<recover_enq_insert_multiple_row_processor *>(extra);
+                        return e->process(dest_key, dest_val);
+                    }
+                } processor(tuple, txn, l);
                 if (renv->generate_row_for_put != NULL) {
                     r = renv->generate_row_for_put(db, src_db, &dest_key, &dest_val, &src_key, &src_val);
                     assert(r==0);
-                    processor.process(&dest_key, &dest_val);
+                    r = processor.process(&dest_key, &dest_val);
+                    assert(r == 0);
                 } else {
                     paranoid_invariant(renv->generate_rows_for_put != NULL);
                     r = renv->generate_rows_for_put(db, src_db, &src_key, &src_val, &processor, &processor.call_process);
@@ -1101,23 +1101,6 @@ static int toku_recover_backward_enq_insert_multiple (struct logtype_enq_insert_
     // nothing
     return 0;
 }
-
-struct recover_enq_delete_multiple_row_processor {
-    struct file_map_tuple *m_tuple;
-    TOKUTXN m_txn;
-    struct logtype_enq_delete_multiple *m_l;
-    recover_enq_delete_multiple_row_processor(struct file_map_tuple *tuple, TOKUTXN txn, struct logtype_enq_delete_multiple *l)
-        : m_tuple(tuple), m_txn(txn), m_l(l) {}
-
-    int process(DBT *dest_key) {
-        toku_ft_maybe_delete(m_tuple->ft_handle, dest_key, m_txn, true, m_l->lsn, false);
-        return 0;
-    }
-    static int call_process(void *extra, DBT *dest_key) {
-        recover_enq_delete_multiple_row_processor *e = static_cast<recover_enq_delete_multiple_row_processor *>(extra);
-        return e->process(dest_key);
-    }
-};
 
 static int toku_recover_enq_delete_multiple (struct logtype_enq_delete_multiple *l, RECOVER_ENV renv) {
     int r;
@@ -1151,11 +1134,28 @@ static int toku_recover_enq_delete_multiple (struct logtype_enq_delete_multiple 
             if (r==0) {
                 // We found the cachefile.  (maybe) Do the delete.
                 DB *db = &tuple->fake_db;
-                recover_enq_delete_multiple_row_processor processor(tuple, txn, l);
+
+                struct recover_enq_delete_multiple_row_processor {
+                    struct file_map_tuple *m_tuple;
+                    TOKUTXN m_txn;
+                    struct logtype_enq_delete_multiple *m_l;
+                    recover_enq_delete_multiple_row_processor(struct file_map_tuple *tuple, TOKUTXN txn, struct logtype_enq_delete_multiple *l)
+                        : m_tuple(tuple), m_txn(txn), m_l(l) {}
+
+                    int process(DBT *dest_key) {
+                        toku_ft_maybe_delete(m_tuple->ft_handle, dest_key, m_txn, true, m_l->lsn, false);
+                        return 0;
+                    }
+                    static int call_process(void *extra, DBT *dest_key) {
+                        recover_enq_delete_multiple_row_processor *e = static_cast<recover_enq_delete_multiple_row_processor *>(extra);
+                        return e->process(dest_key);
+                    }
+                } processor(tuple, txn, l);
                 if (renv->generate_row_for_del != NULL) {
                     r = renv->generate_row_for_del(db, src_db, &dest_key, &src_key, &src_val);
                     assert(r==0);
-                    processor.process(&dest_key);
+                    r = processor.process(&dest_key);
+                    assert(r == 0);
                 } else {
                     paranoid_invariant(renv->generate_rows_for_del != NULL);
                     r = renv->generate_rows_for_del(db, src_db, &src_key, &src_val, &processor, &processor.call_process);
